ffde91c575709146269f8457f8b8d514163d9471
Reniel Ocampo <renielxocampo@gmail.com>
2021-11-04 21:39:44 -0700

    add logo

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Constants from 'expo-constants'
 import { Text, StyleSheet, View } from 'react-native'
+import theme from '../theme';
 
 const Main = () => {
   return (
     <View style={styles.container}>
-      <Text>Raffle Application</Text>
+      <Text>
+          <Text style={[styles.heading, styles.title]}>Raffle</Text>
+          <Text style={[styles.heading, styles.period]}>.</Text>
+        </Text>
     </View>
   );
 };
@@ -15,6 +19,20 @@ const styles = StyleSheet.create({
     marginTop: Constants.statusBarHeight,
     flexGrow: 1,
     flexShrink: 1,
+    backgroundColor: theme.colors.offwhite,
+    padding: 20,
+  },
+  heading: {
+    fontWeight: theme.fontWeights.bold,
+  },
+  title: {
+    fontSize: 40,
+    color: theme.colors.pink,
+    textTransform: 'lowercase',
+  },
+  period: {
+    color: theme.colors.darkBlue,
+    fontSize: 50,
   },
 });
 
---
+++ src/theme.js
@@ -0,0 +1,22 @@
+
+const theme = {
+  colors: {
+    darkBlue: '#424761',
+    offwhite: '#FEFFFE',
+    pink: '#FF556D',
+    cyan: '#1597B0',
+    white: '#FFF',
+  },
+  fontSizes: {
+    body: 18,
+    logo: 40,
+    heading: 30,
+    subheading: 20,
+  },
+  fontWeights: {
+    normal: '400',
+    bold: '700',
+  },
+};
+
+export default theme;
